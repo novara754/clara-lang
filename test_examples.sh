c4ffa97c7d8faa72d3dd2f4d73269c670eceaf1b
vzwGrey <vzwgrey@gmail.com>
2022-06-05 17:01:52 +0200

    test: also link into executable during tests

--- test_examples.sh
+++ test_examples.sh
@@ -6,7 +6,12 @@ cargo build
 for file in ./examples/*; do
   printf "$file: "
   if ./target/debug/clara "$file" &>/dev/null; then
-    echo "OK"
+    if gcc -no-pie -o ./build/a.out ./build/out.o ./lib/clara.c &>/dev/null; then
+      echo "OK"
+    else
+      echo "ERR"
+      exit 1
+    fi
   else
     echo "ERR"
     exit 1
