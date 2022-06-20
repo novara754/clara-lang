7cbae6fef12eeef62e838dd0f1b65e9a5609744e
vzwGrey <vzwgrey@gmail.com>
2022-06-20 19:28:58 +0200

    test: show stderr o failed test, show summary
    When running the test_examples.sh script it will now show the
    Clara compiler's output and GCC's output on failure.
    Failing to compile one file also does not stop further tests anymore.
    A summary is also provided now

--- test_examples.sh
+++ test_examples.sh
@@ -1,19 +1,32 @@
 #! /bin/bash
-set -e
-
 cargo build
 
+TESTS_RUN=0
+TESTS_SUCCESSFUL=0
+EXIT_CODE=0
+
 for file in ./examples/*; do
+  ((TESTS_RUN += 1))
+
   printf "$file: "
-  if ./target/debug/clara "$file" &>/dev/null; then
-    if gcc -no-pie -o ./build/a.out ./build/out.o ./lib/clara.c &>/dev/null; then
+
+  CLARAC_OUT=`./target/debug/clara "$file" 2>&1`
+
+  if [ $? -eq 0 ]; then
+    GCC_OUT=`gcc -no-pie -o ./build/a.out ./build/out.o ./lib/clara.c  2>&1`
+    if [ $? -eq 0 ]; then
+     ((TESTS_SUCCESSFUL += 1))
       echo "OK"
     else
-      echo "ERR"
-      exit 1
+      printf "LINK ERR\n$GCC_OUT\n"
+      EXIT_CODE=1
     fi
   else
-    echo "ERR"
-    exit 1
+    printf "COMPILE ERR\n$CLARAC_OUT"
+    EXIT_CODE=1
   fi
 done
+
+printf "\n$TESTS_SUCCESSFUL/$TESTS_RUN tests succesfull.\n"
+
+exit $EXIT_CODE
