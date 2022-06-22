c9090477ec1d23655b899e8d47d7d307df9d96fc
vzwGrey <vzwgrey@gmail.com>
2022-06-22 16:20:44 +0200

    test: add expected output for each code example
    This adds a file containing the expected output for most
    code examples. The expected output is compared to the actual output
    when running test_examples.sh. This allows some better automated
    testing.

--- test_examples.sh
+++ test_examples.sh
@@ -10,18 +10,32 @@ TESTS_RUN=0
 TESTS_SUCCESSFUL=0
 EXIT_CODE=0
 
-for file in ./examples/*; do
+for FILE in ./examples/*.clara; do
   ((TESTS_RUN += 1))
 
-  printf "$file: "
+  printf "$FILE: "
 
-  CLARAC_OUT=`./target/debug/clara "$file" 2>&1`
+  CLARAC_OUT=`./target/debug/clara "$FILE" 2>&1`
 
   if [ $? -eq 0 ]; then
     GCC_OUT=`gcc -no-pie -o ./build/a.out ./build/out.o ./lib/clara.c  2>&1`
     if [ $? -eq 0 ]; then
-     ((TESTS_SUCCESSFUL += 1))
-      echo "OK"
+      EXPECTED_STDOUT_FILE="${FILE%.*}.stdout"
+      if [ -f "$EXPECTED_STDOUT_FILE" ]; then
+        EXPECTED_STDOUT=`cat "$EXPECTED_STDOUT_FILE"`
+        ACTUAL_STDOUT=`./build/a.out`
+
+        if [ "$EXPECTED_STDOUT" = "$ACTUAL_STDOUT" ]; then
+          ((TESTS_SUCCESSFUL += 1))
+          echo "OK"
+        else
+          printf "INCORRECT OUTPUT\n"
+          printf "$ACTUAL_STDOUT" | diff - "$EXPECTED_STDOUT_FILE"
+        fi
+      else
+        ((TESTS_SUCCESSFUL += 1))
+        echo "OK"
+      fi
     else
       printf "LINK ERR\n$GCC_OUT\n"
       EXIT_CODE=1
